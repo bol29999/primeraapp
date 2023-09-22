ace3e4db7379378efbb06a031c488c094d438aa4
Estela Florez <bol29999@educastur.es>
2023-09-22 19:25:51 +0200

    Primera version

---
+++ src/main/java/pa/cifpaviles/dam/primeraaplicacion/PrimeraAplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pa.cifpaviles.dam.primeraaplicacion;
+
+/**
+ *
+ * @author estelafm
+ */
+public class PrimeraAplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
